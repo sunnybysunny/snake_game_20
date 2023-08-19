cd7dbb41e7b15fefcc1faee3135776b28169a3ab
Sunny <1sunnymuniz@gmail.com>
2023-08-18 23:35:21 -0600

    modified methods to move snake

--- snake.py
+++ snake.py
@@ -3,12 +3,17 @@ from turtle import Turtle
 # Constant variable
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 
 class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         for pos in STARTING_POSITIONS:
@@ -23,21 +28,21 @@ class Snake:
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
 
-    # directions = {"up": 90, "left": 180, "down": 270, "right": 0}
+
     def up(self):
-        self.segments[0].setheading(90)
-        self.move()
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
 
     def down(self):
-        self.segments[0].setheading(270)
-        self.move()
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
 
     def left(self):
-        self.segments[0].setheading(180)
-        self.move()
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
 
     def right(self):
-        self.segments[0].setheading(0)
-        self.move()
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
